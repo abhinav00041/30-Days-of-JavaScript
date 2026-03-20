7a8136dcb3f5de4957dc213895bf9bbbb267405b
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-20 00:18:02 +0000

    Delete solutions directory

--- solutions/easy/Create Hello World Function.js
+++
@@ -1,6 +0,0 @@
-// Title: Create Hello World Function
-            // Difficulty: Easy
-            // Language: JavaScript
-            // Link: https://leetcode.com/problems/create-hello-world-function/
-
-/
