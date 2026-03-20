e8c7808b74495f63178340baee5ad20d7124563b
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-20 00:55:20 +0000

    Add solution for Add Two Promises

--- solutions/easy/Add Two Promises.js
+++ solutions/easy/Add Two Promises.js
@@ -3,20 +3,20 @@
             // Language: JavaScript
             // Link: https://leetcode.com/problems/add-two-promises/
 
-/**
- * @param {Promise} promise1
- * @param {Promise} promise2
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-};
-
-/**
-    })
-          resolve(P1+P2);
      let P1 = await promise1;
      let P2 = await promise2;
     return new Promise(resolve => {
+          resolve(P1+P2);
+    })
+};
+
+/**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
- */
+ * @param {Promise} promise2
+ * @param {Promise} promise1
+/**
+            promise1.then(promise2)
