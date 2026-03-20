b00660b7a540a6689817b70b25c5eccc9c69237b
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-20 00:25:59 +0000

    Add solution for Create Hello World Function

---
+++ solutions/easy/Create Hello World Function.js
@@ -0,0 +1,14 @@
+// Title: Create Hello World Function
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/create-hello-world-function/
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
