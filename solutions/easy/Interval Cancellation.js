d8c5e6a4637b7834ad56d0378e74b69d428eccde
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-22 22:25:43 +0000

    Add solution for Interval Cancellation

---
+++ solutions/easy/Interval Cancellation.js
@@ -0,0 +1,17 @@
+// Title: Interval Cancellation
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/interval-cancellation/
+
+    }
+        clearInterval(timerfn);
+    return function(){
+    let timerfn = setInterval(()=>{fn(...args)},t)
+var cancellable = function(fn, args, t) {
+ */
+ * @return {Function}
+ * @param {number} t
+ * @param {Array} args
+ * @param {Function} fn
+/**
+    fn(...args)
