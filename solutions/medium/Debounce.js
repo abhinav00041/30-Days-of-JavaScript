49a303982166ca339f894d232e8ef486236d608f
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-24 23:29:36 +0000

    Add solution for Debounce

---
+++ solutions/medium/Debounce.js
@@ -0,0 +1,20 @@
+// Title: Debounce
+            // Difficulty: Medium
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/debounce/
+
+    }
+        clearTimeout(timer);
+    return function (...args) {
+};
+
+/**
+ * const log = debounce(console.log, 100);
+        timer = setTimeout(()=>fn(...args),t);
+    let timer;
+var debounce = function (fn, t) {
+ */
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
