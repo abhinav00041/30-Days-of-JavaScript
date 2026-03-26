6b237a58893ab4bc9403f546314736afb6b0997c
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-26 00:01:09 +0000

    Add solution for Execute Asynchronous Functions in Parallel

---
+++ solutions/medium/Execute Asynchronous Functions in Parallel.js
@@ -0,0 +1,22 @@
+// Title: Execute Asynchronous Functions in Parallel
+            // Difficulty: Medium
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/execute-asynchronous-functions-in-parallel/
+
+                    resolve(promiseArray)
+                promiseArray[idx] = result
+                const result = await el();
+        functions.forEach(async(el, idx) => {
+            try{    
+                }
+        let count = 0;         
+                if(functions.length===count){
+        let promiseArray  = [];  
+    return new Promise((resolve, reject) => {
+var promiseAll =async function(functions) {
+                count++
+            }    
+            catch (err) {
+                reject(err)
+            }
+
