0778f61a164d2d88274569782eb9fd39a43e2ef7
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2026-03-27 23:47:59 +0000

    Add solution for Chunk Array

---
+++ solutions/easy/Chunk Array.js
@@ -0,0 +1,18 @@
+// Title: Chunk Array
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/chunk-array/
+
+  let index = 0;
+
+
+  while (index < arr.length) {
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
+  }
+      const chunkedArray = [];
+var chunk = function(arr, size) {
+ */
+  return chunkedArray;
+};
+
